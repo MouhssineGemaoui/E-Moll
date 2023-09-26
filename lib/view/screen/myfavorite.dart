7986d9f7fb432d23e46895e78cfaedc4ae2e4c86
MouhssineGemaoui <117443378+MouhssineGemaoui@users.noreply.github.com>
2023-09-26 16:54:26 +0200

    Add files via upload
    Signed-off-by: MouhssineGemaoui <117443378+MouhssineGemaoui@users.noreply.github.com>

---
+++ lib/view/screen/myfavorite.dart
@@ -0,0 +1,57 @@
+import 'package:ecommercecourse/controller/home_controller.dart';
+import 'package:ecommercecourse/controller/myfavoritecontroller.dart';
+import 'package:ecommercecourse/core/class/handlingdataview.dart';
+import 'package:ecommercecourse/core/constant/routes.dart';
+import 'package:ecommercecourse/view/widget/customappbar.dart'; 
+import 'package:ecommercecourse/view/widget/myfavorite/customlistfavoriteitems.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MyFavorite extends StatelessWidget {
+  const MyFavorite({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+
+    Get.put(MyFavoriteController()); 
+      HomeControllerImp controllersearch = Get.put(HomeControllerImp());
+
+    
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: GetBuilder<MyFavoriteController>(
+            builder: ((controller) => ListView(children: [
+           CustomAppBar(
+            mycontroller: controllersearch.search!,
+            titleappbar: "Find Product",
+            // onPressedIcon: () {},
+            onPressedSearch: () {
+              controllersearch.onSearchItems();
+            },
+            onChanged: (val) {
+              controllersearch.checkSearch(val);
+            },
+            onPressedIconFavorite: () {
+              Get.toNamed(AppRoute.myfavroite);
+            },
+          ),
+                  SizedBox(height: 20) ,
+                  HandlingDataView(
+                      statusRequest: controller.statusRequest,
+                      widget: GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: controller.data.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2, childAspectRatio: 0.7),
+                        itemBuilder: (context, index) {
+                          return  CustomListFavoriteItems(itemsModel: controller.data[index]) ; 
+                        },
+                      ))
+                ]))),
+      ),
+    );
+  }
+}
